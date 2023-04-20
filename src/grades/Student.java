31385da335faebc66962f3b2f31824cc8fc6afe9
Ezra Marama <ezra.marama@gmail.com>
2023-04-20 09:12:42 -0500

    adding interfaces and abstract classes exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Student {
     private String name;
     private double grade;
-    ArrayList<Integer> grades = new ArrayList<>();
+    private ArrayList<Integer> grades = new ArrayList<>();
 
     public Student () {
     }
